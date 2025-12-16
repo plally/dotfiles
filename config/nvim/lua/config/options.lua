e321ddfce011633b343f94dc62230e52120f2dae
Pierce Lally <plally@experientgroup.com>
2025-12-16 14:17:02 -0500

    add custom title string lua function to set tab titles

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -16,3 +16,33 @@ vim.opt.wrap = false
 
 vim.g.copilot_no_tab_map = true
 vim.g.copilot_enabled = true
+
+vim.opt.title = true
+vim.opt.titlelen = 0
+
+local maxPathLength = 35
+local titlePrefix = "vim "
+
+function CustomTitleString()
+    local workingDir = vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
+    if #workingDir < maxPathLength then
+        return titlePrefix .. workingDir
+    end
+
+    local parts = vim.split(workingDir, "/")
+
+    local newParts = {}
+    local newLength = 0
+    for i = #parts, 1, -1 do
+        newLength = newLength + #parts[i] + 1
+        if newLength > maxPathLength and #newParts > 0 then
+            table.insert(newParts, 1, "...")
+            break
+        end
+        table.insert(newParts, 1, parts[i])
+    end
+
+    return titlePrefix .. table.concat(newParts, "/")
+end
+
+vim.opt.titlestring = "%{v:lua.CustomTitleString()}"
